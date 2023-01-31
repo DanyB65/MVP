217726cfbae00180b86be2ecddb631b05129a795
Dany Briceno <danybriceno@Danys-Air.lan>
2023-01-31 12:01:33 -0600

    get request added

--- server.js
+++ server.js
@@ -9,9 +9,14 @@ app.use(express.static("public"));
 
 const app = express()
 const sql = postgres(process.env.DATABASE_URL)
-
+app.get("/tickets" , (req,res) =>{
+    sql `SELECT * FROM ticketInfo`.then((data) =>{
+        res.json(data)
+    })
+})
 /*
 app crud will go here
+postgres://mvp_5scq_user:SodjLzP6sISUGdqQu8XaBr76WLY1eJlW@dpg-cfcl1gpgp3jokp2uauh0-a.oregon-postgres.render.com/mvp_5scq
 */
 
 app.listen(process.env.PORT,() =>{
